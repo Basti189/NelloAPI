d050050987705987b886c1e480db670bfc4af5ea
Sebastian Wolf <Basti189@users.noreply.github.com>
2018-05-26 17:54:53 +0200

    Update NelloActionEvent.java

--- de/wolfsline/nello/api/event/NelloActionEvent.java
+++ de/wolfsline/nello/api/event/NelloActionEvent.java
@@ -3,6 +3,11 @@ package de.wolfsline.nello.api.events;
 import org.json.simple.JSONObject;
 
 public class NelloActionEvent {
+	
+	public final static int SWIPE = 0;
+	public final static int GEO = 1;
+	public final static int TW = 2;
+	public final static int DENY = 3;
 
 	private String name = "";
 	private String location_id = "";
@@ -12,11 +17,21 @@ public class NelloActionEvent {
 	
 	public NelloActionEvent(JSONObject response) {
 		JSONObject data = (JSONObject) response.get("data");
-		name = data.get("name").toString();
-		location_id = data.get("location_id").toString();
-		user_id = data.get("user_id").toString();
+		name = jsonToString(data.get("name"));
+		//name = data.get("name").toString();
+		location_id = jsonToString(data.get("location_id"));
+		//location_id = data.get("location_id").toString();
+		user_id = jsonToString(data.get("user_id"));
+		//user_id = data.get("user_id").toString();
 		
 		action = response.get("action").toString();
+	}
+	
+	private String jsonToString(Object object) {
+		if (object != null) {
+			return object.toString();
+		}
+		return "";
 	}
 
 	public String getName() {
@@ -33,6 +48,19 @@ public class NelloActionEvent {
 
 	public String getAction() {
 		return action;
+	}
+	
+	public int getActionID() {
+		if (action.equals("swipe") ) {
+			return SWIPE;
+		} else if (action.equals("geo")) {
+			return GEO;
+		} else if (action.equals("tw")) {
+			return TW;
+		} else if (action.equals("deny")) {
+			return DENY;
+		}
+		return -1;
 	}
 
 	@Override
